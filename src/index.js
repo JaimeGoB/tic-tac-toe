4aca2505692be888ff7da84f6fd642a67ed099d1
JaimeGoB <josba93@gmail.com>
2020-08-10 22:57:04 -0500

    Board has been setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
+
+const Square = () => {
+  return (
+    <div style={{
+      backgroundColor: 'gold',
+      margin: 10,
+      padding: 20,
+    }}>Square</div>
+  );
+};
+
+const Board = () => {
+  return (
+    <div style={{
+      backgroundColor: 'skyblue',
+      margin: 10,
+      padding: 20,
+    }}>
+      Board
+      <div className="board-row">
+        <Square /><Square /><Square />
+      </div>
+      <div className="board-row">
+        <Square /><Square /><Square />
+      </div>
+      <div className="board-row">
+        <Square /><Square /><Square />
+      </div>
+    </div>
+  );
+};
 
 const Game = () => {
   return (
-    <div>
-      Hello World
+    <div className="game">
+      Game
+      <Board />
     </div>
   );
 };
