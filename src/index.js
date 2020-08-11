aeb1eec264350cfa9cd5e5620bb5d804df8a0d01
JaimeGoB <josba93@gmail.com>
2020-08-11 12:16:16 -0500

    Game is officially done!

--- src/index.js
+++ src/index.js
@@ -32,10 +32,10 @@ const Board = () => {
     //Make copy of state square array
     const newSquares = [...squares];
 
-    const winnderDeclared = Boolean(winnderDeclared(squares));
+    const winnerDeclared = Boolean(winnerDeclared(squares));
     const squareFilled = Boolean(newSquares[i]);
 
-    if (winnderDeclared || squareFilled) {
+    if (winnerDeclared || squareFilled) {
       return;
     }
 
